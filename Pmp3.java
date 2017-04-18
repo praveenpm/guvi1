e0d09566bd8629531e40ee6e455c549449a4b335
praveenpm <praveenpalanisamy227@gmail.com>
2017-04-18 15:24:11 +0530

    Add files via upload

---
+++ Pmp3.java
@@ -0,0 +1,17 @@
+class Pmp3{
+public static void main(String args []){
+int a=Integer.parseInt(args[0]);
+if(a<0)
+{
+System.out.println("a is negative");
+}
+else if(a>0)
+{
+System.out.println("a is pasitive");
+}
+else
+{
+System.out.println(" a is zero ");
+}
+}
+}

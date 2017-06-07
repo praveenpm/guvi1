8fee751072a0da37536a5b21d24721b6e9ee76d2
praveenpm <praveenpalanisamy227@gmail.com>
2017-06-07 12:22:21 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class Loops {
+      public static void main(String[] args) {
+            System.out.println("Let's compact the code using for loop.");
+            int i;
+            for (i = 0; i <5; i++)
+                  System.out.println("Hello!");
+            System.out.println("Now the code looks much better.");
+      }
+}

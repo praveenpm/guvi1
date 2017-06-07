a230aae833a28cd4e0d10892b2776ce1bde07a8f
praveenpm <praveenpalanisamy227@gmail.com>
2017-06-07 15:45:22 +0530

    Add files via upload

---
+++ digitde.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int number,rem,count=0;
+scanf("%d"&n);
+while(n>0)
+{
+number=number/10;
+count=count+1;
+}
+printf("number of digit in a given number=%d",count);
+return 0;
+}

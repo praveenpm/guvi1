1818a9be12345e3a7471bc83e327236ddcca91e1
praveenpm <praveenpalanisamy227@gmail.com>
2017-06-07 14:34:51 +0530

    Add files via upload

---
+++ llo.java
@@ -0,0 +1,7 @@
+public class Loops {
+      public static void main(String[] args) {
+           int i;
+            for (i = 0; i<5; i++)
+                  System.out.println("Hello!");
+     }
+}

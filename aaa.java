950775eed1d69efc124b67854d2281a6ecfbf76a
praveenpm <praveenpalanisamy227@gmail.com>
2017-07-20 10:45:06 +0530

    Add files via upload

---
+++ aaa.java
@@ -0,0 +1,20 @@
+
+public class Bbb
+{
+	 public static void main(String[] args) 
+	 {
+	      String s1="hello";
+	      for( int i=0;i<=s1.length();i++)
+	      {
+	         char e=s1.charAt(i);
+	         if((e== 'A')||(e=='E')||(e=='I')||(e=='O')||(e=='U'))
+	         {
+	             String h=s1.substring(0, i);
+	             String g=s1.substring(i+ i);
+	             s1=h+"Z"+g;
+	         }
+	     }   
+	    System.out.println("The result is="+s1);	                
+    }
+
+}
